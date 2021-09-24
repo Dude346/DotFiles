9c93298c56b847845cf2e4e635ea7ce44b98cf0e
Raja <jiddu100@gmail.com>
2021-09-24 17:48:26 -0400

    Linking the zshrc and zshrc.zwc file

--- startup.zsh
+++ startup.zsh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
+DOT="$HOME/dotfiles" 
 mv "$HOME/.zshrc" "$HOME/.zshrc_original"   
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc    
+[[ ! -f $DOT/.zshrc ]] || ln -s "$DOT/.zshrc" "$HOME/.zshrc" 
+
+[[ ! -f $DOT/.zshrc.zwc ]] || ln -s "$DOT/.zshrc.zwc" "$HOME/.zshrc.zwc" 
 
